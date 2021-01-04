26235ddf86990274801804cca9c2ec19605fe2cc
Ben Stoller <stollersystemsound@gmail.com>
2021-01-04 14:01:54 -0800

    start project

---
+++ src/components/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import { CssBaseline } from '@material-ui/core';
+import Header from './Header';
+import Projects from './Projects'
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    minHeight: '100vh',
+    backgroundImage: `url(${process.env.PUBLIC_URL + '/assets/bluesun1.jpg'})`,
+    backgroundRepeat: 'no-repeat',
+    backgroundSize: 'cover'
+  },
+}));
+
+export default function App() {
+  const classes = useStyles();
+  return <div className={classes.root}>
+    <CssBaseline />
+    <Header />
+    <Projects />
+  </div>
+}
---
+++ src/components/Header.js
@@ -0,0 +1,84 @@
+import React, { useEffect, useState } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import { AppBar, IconButton, Toolbar, Collapse } from '@material-ui/core';
+import SortIcon from '@material-ui/icons/Sort';
+import ArrowDownwardIcon from '@material-ui/icons/ArrowDownward';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+
+    height: '100vh',
+    color: 'white'
+  },
+  appbar: {
+    background: 'none',
+    fontFamily: ''
+  },
+  appbarTitle: {
+    flexGrow: '1'
+  },
+  icon: {
+    color: 'white',
+    fontSize: '2rem',
+  },
+  appbarWrapper: {
+    width: '80%',
+    margin: '0 auto'
+  },
+  colorText: {
+    color: ' #00fbff'
+  },
+  container: {
+    textAlign: 'center',
+  },
+  title: {
+    fontSize: '3.5rem'
+  },
+  goDown: {
+    color: 'white',
+    fontSize: '3rem'
+  }
+}));
+
+export default function Header() {
+
+  const classes = useStyles();
+
+  const [checked, setChecked] = useState(false);
+
+  useEffect(() => {
+    setChecked(true);
+  },[])
+
+
+  return (
+    <div className={classes.root}>
+      <AppBar className={classes.appbar}>
+        <Toolbar className={classes.appbarWrapper}>
+
+          <h1 className={classes.appbarTitle}>STOLLER <span className={classes.colorText}>SYSTEM</span></h1>
+          <IconButton>
+            <SortIcon className={classes.icon} />
+          </IconButton>
+        </Toolbar>
+      </AppBar>
+      <Collapse
+        in={checked}
+        {...(checked ? { timeout: 1000 } : {})}
+        collapsedHeight={50}
+      >
+        <div className={classes.container}>
+          <h1 className={classes.title}>
+            Welcome to <span className={classes.colorText}>STOLLERSYSTEM</span>
+          </h1>
+          <IconButton>
+            <ArrowDownwardIcon className={classes.goDown} />
+          </IconButton>
+        </div>
+      </Collapse>
+    </div>
+  )
+};
